2be808b355f015fd5d72084db81462064309f4a6
joelqui <joelkeequi@gmail.com>
2017-09-11 11:27:32 +0800

    corrections for connections to the db.

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli('127.0.0.1','root','','data_mix');
+$db = new mysqli('127.0.0.1','root','','deped_southern_leyte');
 
 if($db->connect_errno !== 0){
     die('We are down at the moment, try again soon.');
